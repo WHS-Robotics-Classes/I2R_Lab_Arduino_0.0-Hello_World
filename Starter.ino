c7ce550205d8075320b65c88983b2e9253692a3f
Steve Cline <steve.cline@weldre4.org>
2021-01-08 14:20:36 -0700

    Create Starter.ino

---
+++ Starter.ino
@@ -0,0 +1,14 @@
+// Create a sketch that prints "Hello, World:" followed by a counter that continually climbs by one number at a time.
+// Use this code to get started.
+
+int x = 0;
+
+void setup() {
+  Serial.begin();
+}
+
+void loop() {
+  S_____.____("Hello, World: ");
+  S_____.____(x);
+  x = __ + __
+}
