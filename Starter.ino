118f7b5d6f5c107ff8cce03deb7449f5c45cd2fd
Steve Cline <steve.cline@weldre4.org>
2023-02-06 10:16:38 -0700

    Update Starter.ino

--- Starter.ino
+++ Starter.ino
@@ -1,7 +1,7 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println(); // put your message that you would like to display in the serial monitor in quotes inside the parenthesis here
+  Serial.println(); // put your message that you would like to display in the serial monitor in quotes inside the parenthesis here.  For example, if you want to print the name of our school, you would put "Windsor High School" in the parenthesis.
 }
 
 void loop() {
